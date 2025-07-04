4c024b6de7d48a8a8f8765e8d3825158b54dac0e
Koswara <koswara.c79@gmail.com>
2025-07-05 05:52:37 +0700

    fix import multi lang

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import I18nProvider from '../components/I18nProvider';
+import I18nProvider from '@/components/I18nProvider';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
--- src/components/I18nProvider.tsx
+++ src/components/I18nProvider.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { I18nextProvider } from 'react-i18next';
-import i18n from '../i18n'; // Import your i18n configuration
+import i18n from '@/i18n'; // Import your i18n configuration
 
 interface I18nProviderProps {
   children: React.ReactNode;
